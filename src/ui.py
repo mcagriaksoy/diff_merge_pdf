4bef609853369fbd13867644a058fc068f3aef56
Mehmet Cagri Aksoy <mcagriaksoy@yandex.com>
2024-01-04 21:58:52 +0100

    first displaying setting

--- src/ui.py
+++ src/ui.py
@@ -2,6 +2,7 @@ __author__ = 'Mehmet Cagri Aksoy - github.com/mcagriaksoy'
 
 import sys
 import os
+import pdfplumber
 
 from PyQt6.QtWidgets import QApplication, QMainWindow, QFileDialog
 from PyQt6.uic import loadUi
@@ -26,15 +27,28 @@ class MainWindow(QMainWindow):
         if file_path:
             return file_path
 
+    def open_pdf(self, file_path, left_or_right):
+        """ Open PDF """
+        if file_path:
+            index = 0
+            with pdfplumber.open(file_path) as pdf:
+                if left_or_right == "left":
+                    self.textBrowser.append(pdf.pages[index].extract_text())
+                else:
+                    self.textBrowser_2.append(pdf.pages[index].extract_text())
+                index += 1
+
     def left_file_dialog(self):
         """ Left File Dialog """
         file_path = self.open_file_dialog()
         self.lineEdit.setText(file_path)
+        self.open_pdf(file_path, "left")
 
     def right_file_dialog(self):
         """ Right File Dialog """
         file_path = self.open_file_dialog()
         self.lineEdit_2.setText(file_path)
+        self.open_pdf(file_path, "right")
 
 
 def start_ui_design():
